62e7f9d52e33b23f8f507175b7f55edfa1689859
Dominik Jungowski <Dominik@Jungowski.de>
2015-02-20 00:15:46 +0100

    call map 5 times

--- src/mock-reduce.js
+++ src/mock-reduce.js
@@ -1,1 +1,17 @@
 MockReduce = function() {};
+
+MockReduce.prototype._nextTestData = null;
+
+MockReduce.prototype.setNextTestData = function (nextTestData) {
+	this._nextTestData = nextTestData;
+};
+
+MockReduce.prototype.run = function () {
+	for (var key in this._nextTestData) {
+		this.map();
+	}
+};
+
+MockReduce.prototype.map = function () {
+
+};
--- test/mock-reduce-test.js
+++ test/mock-reduce-test.js
@@ -2,4 +2,23 @@ describe('Mock Reduce Test', function() {
     it('exists', function() {
         expect(MockReduce).not.toBeUndefined();
     });
+
+	describe('#map', function() {
+	    it('is called for every element of the data set', function() {
+	        var mockReduce = new MockReduce();
+			spyOn(mockReduce, 'map').and.callThrough();
+
+			var mockData = [
+				{first: "data"},
+				{second: "data"},
+				{third: "data"},
+				{fourth: "data"},
+				{fifth: "data"}
+			];
+
+			mockReduce.setNextTestData(mockData);
+			mockReduce.run();
+			expect(mockReduce.map.calls.count()).toEqual(5);
+	    });
+	});
 });

a67a3e0528b04695b5345baeaba4c3f6dfd2d265
Dominik Jungowski <Dominik@Jungowski.de>
2015-02-20 15:41:24 +0100

    Uninstall for mongoose

--- spec/src/installer-spec.js
+++ spec/src/installer-spec.js
@@ -40,5 +40,18 @@ describe('Install spec', function () {
 			this.installer.uninstall();
 			expect(mongoDbMock.connect).toBe(originalConnect);
 	    });
+
+		it('reinstalls the original connect and createConnection', function() {
+			var mongoDbMock = {
+				connect: function() {},
+				createConnection: function() {}
+			};
+			var originalConnect = mongoDbMock.connect;
+			var originalCreateConnection = mongoDbMock.createConnection;
+			this.installer.install(mongoDbMock);
+			this.installer.uninstall();
+			expect(mongoDbMock.connect).toBe(originalConnect);
+			expect(mongoDbMock.createConnection).toBe(originalCreateConnection);
+		});
 	});
 });
--- src/installer.js
+++ src/installer.js
@@ -2,6 +2,7 @@ MockReduce.Installer = function() {};
 
 MockReduce.Installer.prototype._connector = null;
 MockReduce.Installer.prototype._originalConnect = null;
+MockReduce.Installer.prototype._originalCreateConnection = null;
 
 MockReduce.Installer.prototype.install = function(connector) {
 	this._connector = connector;
@@ -9,10 +10,12 @@ MockReduce.Installer.prototype.install = function(connector) {
 	connector.connect = function() {};
 
 	if (connector.createConnection != undefined) {
+		this._originalCreateConnection = connector.createConnection;
 		connector.createConnection = function() {};
 	}
 };
 
 MockReduce.Installer.prototype.uninstall = function () {
 	this._connector.connect = this._originalConnect;
+	this._connector.createConnection = this._originalCreateConnection;
 };
